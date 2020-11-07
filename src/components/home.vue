86a656298ecbf2aefc71a0b6d64154ff55e50cc8
yang <1164078162@qq.com>
2020-11-08 02:23:25 +0800

    第一次提交

---
+++ src/components/home.vue
@@ -0,0 +1,360 @@
+<template>
+  <div>
+    <div class="header">
+      <img class="logo" src="../image/logo.png" alt="LOGO" />
+      <div class="text">极速思维AI教育</div>
+      <div class="English">FAST THINKING AI EDUCATION</div>
+      <img class="catalog" src="../image/设计目录.png" alt="目录" />
+    </div>
+    <img
+      class="one-screen"
+      src="../image/默认文件1603988541526.png"
+      alt="第一屏"
+    />
+    <div class="quickness-title-vessle">
+      <div class="title">急速思维AI教育品牌</div>
+      <div class="wire"></div>
+    </div>
+    <div class="quickness-content-vessle">
+      <div class="quickness-content-text">
+        急速思维AI教育是凯文瑞思教育(北京)科技有限公司旗下的学习训练品牌。急速思维AI学习系列课程专注6-18岁孩子学习效率和能力的提升，激发孩子学习潜能，增强孩子学习兴趣与动力，让孩子爱学习，能学习，会学习，为未来持续的学习成长奠定扎实的基础。
+      </div>
+    </div>
+    <div class="quickness-content-image-vessle">
+      <img
+        class="quickness-content-image"
+        src="../image/pexels-ekrulila-2292837.jpg"
+        alt="文凭"
+      />
+    </div>
+    <div class="quickness-title-vessle" style="margin-top: 0.7rem">
+      <div class="title">凯文瑞思教育</div>
+      <div class="wire"></div>
+    </div>
+    <div class="quickness-content-vessle" style="height: 2.2rem">
+      <div class="quickness-content-text">
+        凯文瑞思教育是一家以互联网为核心，以精准培训服务为链接，全面赋能B端，间接服务C端，通过提供特色，盈利，爆品，增值项目和解决方案，帮助B端解决招生难，管理难，盈利雅三大问题，打造B端差异化特色核心竞争力，助其深度挖掘C端客户价值实现系统升级，利润倍增，生态发展的一站式教育服务商。
+      </div>
+    </div>
+    <div class="quickness-content-image-vessle">
+      <img
+        class="quickness-content-image"
+        src="../image/pexels-andrea-piacquadio-3769118.jpg"
+        alt="文凭"
+      />
+    </div>
+    <div class="series-title-vessle">
+      <div class="title">极速思维学习力系列课程</div>
+      <div class="wire"></div>
+      <div class="content">
+        课程,以循序渐进、科学系统地提高学生的学习效率和力。
+      </div>
+    </div>
+    <div class="series-content-vessle">
+      <img src="../image/底图.png" alt="底图" />
+      <div class="bail" style="top: 0.3rem; left: 1rem">
+        <img
+          style="top: 0.5rem; left: 0.85rem"
+          src="../image/速度，增长.png"
+          alt="火箭"
+        />
+        <div class="bail-text" style="bottom: 0.7rem; left: 0rem">
+          《 高倍速读 》
+        </div>
+      </div>
+      <div class="bail" style="top: 0.3rem; right: 1rem">
+        <img
+          style="top: 0.5rem; left: 0.85rem"
+          src="../image/预警.png"
+          alt="火箭"
+        />
+        <div class="bail-text" style="bottom: 0.7rem; left: 0rem">
+          《 极速注意力 》
+        </div>
+      </div>
+      <div class="bail" style="bottom: 0.3rem; left: 1rem">
+        <img
+          style="top: 0.5rem; left: 0.85rem"
+          src="../image/记忆 (1).png"
+          alt="火箭"
+        />
+        <div class="bail-text" style="bottom: 0.7rem; left: 0rem">
+          《 极速记忆力 》
+        </div>
+      </div>
+      <div class="bail" style="bottom: 0.3rem; right: 1rem">
+        <img
+          style="top: 0.5rem; left: 0.85rem"
+          src="../image/思维 (1).png"
+          alt="火箭"
+        />
+        <div class="bail-text" style="bottom: 0.7rem; left: 0rem">
+          《 极速思维导图 》
+        </div>
+      </div>
+    </div>
+    <img
+      class="develop-image"
+      src="../image/默认文件1604073118719.png"
+      alt="文章"
+    />
+    <img
+      class="develop-image"
+      style="margin-top: -0.1rem"
+      src="../image/默认文件16040785760011111.png"
+      alt="文章"
+    />
+    <img
+      class="develop-image"
+      style="margin-top: -0.1rem; height: 20rem"
+      src="../image/默认文件1604328627050.png"
+      alt="文章"
+    />
+    <img
+      class="develop-image"
+      style="margin-top: -0.1rem; height: 30rem"
+      src="../image/未命名.png"
+      alt="文章"
+    />
+    <div class="bottom-vessle">
+      <div class="title">咨询热线</div>
+      <div class="lin"></div>
+      <img class="phone-onw" src="../image/电话.png" alt="电话" />
+      <div class="phone-num-onw">131-6112-8516</div>
+      <img class="phone-tow" src="../image/电话.png" alt="电话" />
+      <div class="phone-num-tow">188-1155-9155</div>
+      <img
+        class="code-image"
+        src="../image/微信图片_20201030012554.png"
+        alt="code"
+      />
+      <div class="topspeed-records">© 极速思维AI教育 京ICP备 ********号</div>
+      <div class="topspeed-records" style="top: 5.75rem; left: 0.5rem">
+        北京******有限公司
+      </div>
+      <div class="topspeed-records" style="top: 6.2rem; left: 0.5rem">
+        京公网安备 ****************号
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.header {
+  width: 100%;
+  height: 1rem;
+  background: #c7132c;
+  position: fixed;
+  top: 0;
+  left: 0;
+  -webkit-box-shadow: 0 0.16rem 0.12rem -0.12rem black;
+  -moz-box-shadow: 0 0.16rem 0.12rem -0.12rem black;
+  box-shadow: 0 0.16rem 0.12rem -0.12rem black;
+  z-index: 1;
+}
+.logo {
+  width: 0.8rem;
+  height: 0.8rem;
+  position: absolute;
+  left: 0.4rem;
+  top: 0.1rem;
+}
+.text {
+  font-size: 0.1rem;
+  position: absolute;
+  left: 1.3rem;
+  top: 0.19rem;
+  color: #ffffff;
+}
+.English {
+  font-size: 0.1rem;
+  position: absolute;
+  left: 1.3rem;
+  top: 0.58rem;
+  color: #ffffff;
+}
+.catalog {
+  width: 0.5rem;
+  height: 0.4rem;
+  position: absolute;
+  top: 0.3rem;
+  right: 0.3rem;
+}
+.one-screen {
+  width: 100%;
+  height: 11rem;
+  margin-top: 1rem;
+}
+.quickness-title-vessle {
+  width: 100%;
+  text-align: center;
+}
+.quickness-title-vessle .title {
+  width: 100%;
+  text-align: center;
+  font-size: 0.3rem;
+}
+.quickness-title-vessle .wire {
+  display: inline-block;
+  width: 1.2rem;
+  height: 0.05rem;
+  background: #c7132c;
+}
+.quickness-content-vessle {
+  width: 100%;
+  height: 1.9rem;
+  position: relative;
+  margin-top: 0.1rem;
+}
+.quickness-content-vessle .quickness-content-text {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  margin: auto;
+  width: 85%;
+  display: inline-block;
+  font-size: 0.26rem;
+  line-height: 0.35rem;
+}
+.quickness-content-image-vessle {
+  width: 100%;
+  text-align: center;
+}
+.quickness-content-image-vessle .quickness-content-image {
+  width: 85%;
+  height: 3.6rem;
+  border-radius: 0.2rem;
+}
+.series-title-vessle {
+  width: 100%;
+  text-align: center;
+  margin-top: 0.5rem;
+}
+.series-title-vessle .title {
+  width: 100%;
+  text-align: center;
+  font-size: 0.3rem;
+}
+.series-title-vessle .wire {
+  display: inline-block;
+  width: 1.2rem;
+  height: 0.05rem;
+  background: #c7132c;
+}
+.series-title-vessle .content {
+  width: 100%;
+  text-align: center;
+  font-size: 0.26rem;
+  color: #c7132c;
+  margin-top: 0.1rem;
+}
+.series-content-vessle {
+  width: 100%;
+  position: relative;
+  height: 6rem;
+}
+.series-content-vessle img {
+  width: 100%;
+  height: 100%;
+}
+.series-content-vessle .bail {
+  width: 2.5rem;
+  height: 2.5rem;
+  border-radius: 50%;
+  box-shadow: 0rem 0rem 0.1rem black;
+  position: absolute;
+  background: #ffffff;
+}
+.series-content-vessle .bail img {
+  width: 0.7rem;
+  height: 0.7rem;
+  position: absolute;
+}
+.series-content-vessle .bail .bail-text {
+  position: absolute;
+  font-size: 0.1rem;
+  width: 100%;
+  text-align: center;
+}
+.develop-image {
+  width: 100%;
+  height: 11rem;
+}
+.bottom-vessle {
+  width: 100%;
+  height: 7rem;
+  background: #333333;
+  margin-top: -0.1rem;
+  position: relative;
+}
+.bottom-vessle .title {
+  font-size: 0.4rem;
+  color: #ffffff;
+  position: absolute;
+  top: 0.3rem;
+  left: 0.3rem;
+}
+.bottom-vessle .lin {
+  width: 0.5rem;
+  height: 0.05rem;
+  background: #c7132c;
+  position: absolute;
+  top: 0.8rem;
+  left: 0.3rem;
+}
+.bottom-vessle .phone-onw {
+  width: 0.6rem;
+  height: 0.6rem;
+  position: absolute;
+  top: 1.3rem;
+  left: 0.6rem;
+}
+.bottom-vessle .phone-num-onw {
+  font-size: 0.5rem;
+  font-weight: bold;
+  color: #ffffff;
+  position: absolute;
+  top: 1.3rem;
+  left: 1.6rem;
+}
+.bottom-vessle .phone-tow {
+  width: 0.6rem;
+  height: 0.6rem;
+  position: absolute;
+  top: 2.2rem;
+  left: 0.6rem;
+}
+.bottom-vessle .phone-num-tow {
+  font-size: 0.5rem;
+  font-weight: bold;
+  color: #ffffff;
+  position: absolute;
+  top: 2.2rem;
+  left: 1.6rem;
+}
+.bottom-vessle .code-image {
+  width: 2rem;
+  height: 2rem;
+  position: absolute;
+  top: 3rem;
+  left: 0.5rem;
+}
+.bottom-vessle .topspeed-records {
+  font-size: 0.1rem;
+  color: #7C7C7C;
+  position: absolute;
+  top: 5.3rem;
+  left: 0.5rem;
+}
+</style>
